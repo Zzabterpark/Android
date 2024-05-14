94adc838a64d6778002bdf899be0d3b60da09af3
희은 <datakubit2@gmail.com>
2024-05-15 03:18:16 +0900

    feat: 홈 화면 배너 레이아웃

--- app/src/main/java/com/example/zzabterpark/MainActivity.kt
+++ app/src/main/java/com/example/zzabterpark/MainActivity.kt
@@ -1,20 +1,17 @@
 package com.example.zzabterpark
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val toolbar = findViewById<View>(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
     }
 }
+
